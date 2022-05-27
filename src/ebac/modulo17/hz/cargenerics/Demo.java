5e15b65325863bfc74aa822397c1242a51a6fcb8
felipehanzava <fhanzava@hotmai.com>
2022-05-27 14:25:17 -0300

    Generics

--- src/ebac/modulo17/hz/cargenerics/Demo.java
+++ src/ebac/modulo17/hz/cargenerics/Demo.java
@@ -1,51 +1,52 @@
 package ebac.modulo17.hz.cargenerics;
 
+import ebac.modulo17.hz.cargenerics.buildercar.GenericBuilder;
 import ebac.modulo17.hz.cargenerics.buildercar.JeepBuilder;
 import ebac.modulo17.hz.cargenerics.buildercar.MercedesBuilder;
 import ebac.modulo17.hz.cargenerics.carros.C180Car;
 import ebac.modulo17.hz.cargenerics.carros.Compass;
+import ebac.modulo17.hz.cargenerics.carros.NivusCar;
 import ebac.modulo17.hz.cargenerics.factory.Car;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Demo<T> {
+public class Demo {
 
-    static C180Car c180Car = new C180Car();
-    static Compass compassCar = new Compass();
+    static Car c180Car = new C180Car();
+    static Car compassCar = new Compass();
+    static Car nivusCar = new NivusCar();
     static List<Car> listaCarro = new ArrayList<>();
     private static Object C180Car;
 
     public static void main(String args[]) {
 
         preencherLista();
-        System.out.println(listaCarro);
-
+        imprimir(listaCarro);
 
     }
+
+    private static void imprimir(List<?> list) {
+        System.out.println(list);
+        }
 
     private static void preencherLista() {
-//        GenericBuilder c180 = genericBuilder.codigo(c180Car.getCodigo())
-//                .marca(c180Car.getMarca())
-//                .nome(c180Car.getNome())
-//                .lugares(c180Car.getLugares());
+        GenericBuilder genericBuilder = new GenericBuilder(c180Car);
+        Car c180 = genericBuilder.build();
 
+        GenericBuilder genericBuilder1 = new GenericBuilder(compassCar);
+        Car compass = genericBuilder.build();
 
+        GenericBuilder genericBuilder2 = new GenericBuilder(nivusCar);
+        Car nivus = genericBuilder.build();
 
-        C180Car c180r = MercedesBuilder.builder().codigo(c180Car.getCodigo())
-                .marca(c180Car.getMarca())
-                .nome(c180Car.getNome())
-                .lugares(c180Car.getLugares()).build();
-
-        Compass compass = JeepBuilder.builder().codigo(compassCar.getCodigo())
-                .marca(compassCar.getMarca())
-                .nome(compassCar.getNome())
-                .lugares(compassCar.getLugares()).build();
-
- //       listaCarro.add(c180);
-        listaCarro.add(c180r);
+        listaCarro.add(nivus);
+        listaCarro.add(c180);
         listaCarro.add(compass);
     }
+
+
+
 
 
 }
--- src/ebac/modulo17/hz/cargenerics/buildercar/GenericBuilder.java
+++ src/ebac/modulo17/hz/cargenerics/buildercar/GenericBuilder.java
@@ -5,48 +5,21 @@ import ebac.modulo17.hz.cargenerics.carros.Compass;
 import ebac.modulo17.hz.cargenerics.carros.NivusCar;
 import ebac.modulo17.hz.cargenerics.factory.Car;
 
-public class GenericBuilder<T> extends Car {
+public class GenericBuilder<T>{
 
-//    T carro;
-//
-//    public GenericBuilder(Class nome) {
-//        String carroClass = nome.getClass().getName();
-//        if (carroClass.compareTo("C180Car") == 0) {
-//            this.carro = (T) new C180Car();
-//        } else if (carroClass.compareTo("Compass") == 0) {
-//            this.carro = (T) new Compass();
-//        } else {
-//            this.carro = (T) new NivusCar();
-//        }
-//
-//    }
-//
-//    public GenericBuilder() {
-//
-//    }
-//
-//    public GenericBuilder<T> codigo(int codigo){
-//        this.carro.setCodigo(codigo);
-//        return this;
-//    }
-//
-//    public GenericBuilder marca(String marca){
-//        this.carro.setMarca(marca);
-//        return this;
-//    }
-//
-//    public GenericBuilder nome(String nome){
-//        this.carro.setNome(nome);
-//        return this;
-//    }
-//
-//    public GenericBuilder lugares(String lugares){
-//        this.carro.setLugares(lugares);
-//        return this;
-//    }
-//
-//    public Car<T> build(){
-//        return this.carro;
-//    }
-//
+    Car carro;
+
+    public GenericBuilder(Car<T> car) {
+         this.carro = car;
+         carro.setCodigo(car.getCodigo());
+         carro.setMarca(car.getMarca());
+         carro.setNome(car.getNome());
+         carro.setLugares(car.getLugares());
+    }
+
+
+    public Car<T> build(){
+        return this.carro;
+    }
+
 }
